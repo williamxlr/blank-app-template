5050a07ff0df2aab0958dbcec707b7446becd3a0
William Alexander Rodriguez Arroyo <35548531+williamxlr@users.noreply.github.com>
2024-10-22 20:14:35 -0500

    Update streamlit_app.py
    000

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start 000! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
